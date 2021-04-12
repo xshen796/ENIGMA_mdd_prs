d1e82af75d7bcd0f88938b8dba4e3068cde359d8
SHEN Xueyi <xshen33@login01.ecdf.ed.ac.uk>
2021-04-12 14:05:34 +0100

    update script for dummy pheno

--- util/create_dummypheno.R
+++ util/create_dummypheno.R
@@ -16,11 +16,11 @@ bim.file=opt$bim
 
 # Make dummy phenotype file -----------------------------------------------
 
-bim.local = read.delim(bim.file,header = F,quote = F,row.names = F,col.names = F,stringsAsFactors = F) 
+bim.local = read.delim(bim.file,header = F,stringsAsFactors = F) 
 
 dummy.pheno = bim.local %>% 
   select(FID = V1, IID = V2) %>% 
   mutate(dummy_pheno = runif(nrow(.),0,1)) %>% 
   mutate(dummy_pheno = round(dummy_pheno))
 
-write.table(dummy.pheno,file='data/dummy_pheno.txt')
+write.table(dummy.pheno,file='data/dummy_pheno.txt',quote=F,col.names=F,row.names=F)
